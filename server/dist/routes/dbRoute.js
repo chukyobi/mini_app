e5a8447b618d93b3e234be1f6a891ed9a0b21c16
Chukwudi Obi <162724360+chukyobi@users.noreply.github.com>
2024-07-25 14:20:19 -0700

    Delete server/dist directory

--- server/dist/routes/dbRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dbController_1 = require("../controllers/dbController");
-const router = (0, express_1.Router)();
-router.get("/getUserData", dbController_1.getUserData);
-exports.default = router;
